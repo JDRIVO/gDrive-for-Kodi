4cb1d656b7b030237e9f46540580f8209c71744f
GangaBanga <41508421+GangaBanga@users.noreply.github.com>
2020-12-30 02:23:23 +1100

    Front end update

--- resources/lib/streamer.py
+++ resources/lib/streamer.py
@@ -122,7 +122,90 @@ class myStreamer(BaseHTTPRequestHandler):
 			for r in re.finditer('client_id\=([^\&]+)\&\client_secret\=([^\&]+)', post_data, re.DOTALL):
 				client_id = r.group(1)
 				client_secret = r.group(2)
-				self.wfile.write('<html><head><meta name="viewport" content="width=device-width, initial-scale=1"></head><body>Two steps away.<br/><br/>1) Visit this site and then paste the application code in the below form: <a href="https://accounts.google.com/o/oauth2/auth?scope=https://www.googleapis.com/auth/drive&redirect_uri=urn:ietf:wg:oauth:2.0:oob&response_type=code&client_id=' + str(client_id) + '" target="new">Google Authentication</a><br/><br/>2) Return back to this tab and provide a nickname and the application code provided in step 1.<br/><br/><form action="/enroll" method="post">Nickname for account:<br/><input type="text" name="account"><br/>Code (copy and paste from step 1):<br/><input type="text" name="code"><br/><form action="/enroll" method="post">Client ID:<br/><input type="text" name="client_id" value="' + str(client_id) + '"><br/>Client Secret:<br/><input type="text" name="client_secret" value="' + str(client_secret) + '"><br/><br/> <input type="submit" value="Submit"></form></body></html>')
+				self.wfile.write('''
+				<html>
+				<head>
+				<meta name="viewport" content="width=device-width, initial-scale=1">
+				<style>
+				.text {
+								width: 300px;
+								margin: 20 auto;
+				}
+				.container {
+								position: absolute;
+								top: 50%%;
+								left: 50%%;
+								-moz-transform: translateX(-50%%) translateY(-50%%);
+								-webkit-transform: translateX(-50%%) translateY(-50%%);
+								transform: translateX(-50%%) translateY(-50%%);
+								background-color: 080808;
+								border: 1px solid black;
+								height: 200px;
+								width: 300px;
+								background-color: #0F0F0F;
+								box-shadow: 5px 10px;
+				}
+				.inner {
+								position: absolute;
+								top: 50%%;
+								left: 50%%;
+								-moz-transform: translateX(-50%%) translateY(-50%%);
+								-webkit-transform: translateX(-50%%) translateY(-50%%);
+								transform: translateX(-50%%) translateY(-50%%);
+				}
+				.input {
+								width: 200px;
+								height: 26px;
+								border: 0.6px solid black;
+								background-color: #2E2E2E;
+				}
+				.button {
+								width: 200px;
+								height: 26px;
+								margin-top: 20px;
+								border: 0.6px solid black;
+								background-color: #1D1D1D;
+								margin-top: 21px;
+				}
+				a:link {
+								color: white;
+								text-decoration: none;
+								font-family: Arial
+				}
+				a:hover {
+								text-decoration: underline;
+				}
+				a:visited {
+								color: white;
+				}
+				body {
+								background-color: #080808;
+				}
+				</style>
+				</head>
+				<body>
+				<a href="https://accounts.google.com/o/oauth2/auth?scope=https://www.googleapis.com/auth/drive&redirect_uri=urn:ietf:wg:oauth:2.0:oob&response_type=code&client_id=%s" target="_blank">
+				<div class="text">
+				Click here and paste the code in the form below and then enter an account name.
+				</div>
+				</a>
+				<div class="container">
+				<form action="/enroll" method="post">
+				<div class="inner">
+				<input div class="input" style="color:white" type="text" name="account" placeholder="Account name">
+				<br/>
+				<br/>
+				<input div class="input" style="color:white" type="text" name="code" placeholder="Google code">
+				<br/>
+				<input div class="button" style="color:white" type="submit" value="Submit">
+				<input hidden div class="input" style="color:white" type="text" name="client_id" value="%s" placeholder="Client ID">
+				<input hidden div class="input" style="color:white" type="text" name="client_secret" value="%s" placeholder="Client secret">
+				</div>
+				</div>
+				</form>
+				</div>
+				</body>
+				</html>''' % (client_id, client_id, client_secret) )
 
 		# redirect url to output
 		elif self.path == '/enroll':
@@ -421,39 +504,52 @@ class myStreamer(BaseHTTPRequestHandler):
 							-moz-transform: translateX(-50%) translateY(-50%);
 							-webkit-transform: translateX(-50%) translateY(-50%);
 							transform: translateX(-50%) translateY(-50%);
-							background-color: black;
-
+							background-color: 080808;
+							border: 1px solid black;
+							height: 200px;
+							width: 300px;
+							background-color: #0F0F0F;
+							box-shadow: 5px 10px;
+			}
+			.inner {
+							position: absolute;
+							top: 50%;
+							left: 50%;
+							-moz-transform: translateX(-50%) translateY(-50%);
+							-webkit-transform: translateX(-50%) translateY(-50%);
+							transform: translateX(-50%) translateY(-50%);
 			}
 			.input {
 							width: 200px;
 							height: 26px;
-							border: 1px solid white;
-							background-color: #f6f6f6;
+							border: 0.6px solid black;
+							background-color: #2E2E2E;
 			}
 			.button {
 							width: 200px;
 							height: 26px;
-							margin-top: 20px;
-							text-align: center;
-							border: 1px solid black;
-							background-color: #29292A;
+							border: 0.6px solid black;
+							background-color: #1D1D1D;
+							margin-top: 21px;
 			}
 			body {
-							background-color: black;
+							background-color: #080808;
 			}
 			</style>
 			</head>
 			<body>
 			<div class="container">
 			<form action="/enroll?default=false" method="post">
-			<input div class="input" type="text" name="client_id" placeholder="Client ID">
+			<div class="inner">
+			<input div class="input" style="color:white" type="text" name="client_id" placeholder="Client ID">
 			<br/>
 			<br/>
-			<input div class="input" type="text" name="client_secret" placeholder="Client Secret">
+			<input div class="input" style="color:white" type="text" name="client_secret" placeholder="Client Secret">
 			<br/>
 			<input div class="button" style="color:white" type="submit" value="Submit">
-			</form>
 			</div>
+			</div>
+			</form>
 			</body>
 			</html>''')
 			return
